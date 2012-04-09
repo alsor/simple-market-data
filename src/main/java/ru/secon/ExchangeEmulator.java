44cbe83ab8517c6ea65b3e17f476a200bc419724
alsor <alsor.net@gmail.com>
2012-04-09 17:55:18 +0400

    now we can generate add order messages into file

--- src/main/java/ru/secon/ExchangeEmulator.java
+++ src/main/java/ru/secon/ExchangeEmulator.java
@@ -2,13 +2,22 @@ package ru.secon;
 
 import static ru.secon.AsciiByteUtils.putInt;
 import static ru.secon.AsciiByteUtils.putIntAsFloat;
+import static ru.secon.ExchangeEmulator.Order.BUY;
+import static ru.secon.ExchangeEmulator.Order.SELL;
 import static ru.secon.ExchangeEmulator.Order.order;
+import static ru.secon.ExchangeEmulator.Symbol.symbol;
 import static ru.secon.Utils.doubleAsInt;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 
 public class ExchangeEmulator {
@@ -19,7 +28,13 @@ public class ExchangeEmulator {
 	public static final int ID_LENGTH = 10;
 	public static final int SYMBOL_LENGTH = 6;
 	public static final int QTY_LENGTH = 6;
-	public static final int FILE_BUFFER_SIZE = 1024;
+	public static final int FILE_BUFFER_SIZE = 1024 * 1024;
+	public static final int SYMBOLS_SIZE = 500;
+	public static final int PRICE_POWERS = 7;
+	public static final int MAX_QUANTITY = 100000;
+	public static char[] letters = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
+			'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Z', 'Y',
+			'Z' };
 
 	private int id = 1;
 
@@ -75,22 +90,78 @@ public class ExchangeEmulator {
 		putInt(buf, order.qty, QTY_LENGTH);
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		generateSymbols();
+
 		FileChannel channel = new FileOutputStream(new File("data.txt")).getChannel();
 		ExchangeEmulator emulator = new ExchangeEmulator();
 		ByteBuffer buf = ByteBuffer.allocate(FILE_BUFFER_SIZE);
+
 		
-		emulator.placeOrder(
-				buf,
-				order.symbol(randomSymbol()).side(randomSide()).price(randomPrice())
-						.qty(randomQty()));
-		buf.put(NL);
-		
-		channel.write(buf);
+		int msgToGenerate = 1000000;
+		for (int msgCount = 0; msgCount < msgToGenerate; msgCount++) {
+			emulator.placeOrder(buf, order.symbol(randomSymbol()).side(randomSide()).price(randomPrice())
+					.qty(randomQty()));
+			buf.put(NL);
+
+			if (buf.remaining() < MAX_MSG_LENGTH + 1) {
+				buf.flip();
+				channel.write(buf);
+				buf.clear();
+			}
+		}
+		if (buf.position() > 0) {
+			buf.flip();
+			channel.write(buf);
+		}
 		channel.close();
 	}
 
+	private static int randomQty() {
+		return rnd.nextInt(MAX_QUANTITY);
+	}
+
+	private static double randomPrice() {
+		double price;
+		while (true) {
+			int power = rnd.nextInt(PRICE_POWERS);
+			double base = rnd.nextDouble();
+			price = base * power;
+			if (price > 0.0) break;
+		}
+		return price;
+	}
+
+	private static byte randomSide() {
+		if (rnd.nextFloat() > 0.5) {
+			return SELL;
+		}
+		return BUY;
+	}
+
+	private static List<String> symbols;
+	private static Random rnd = new Random();
+
 	private static Symbol randomSymbol() {
-		return null;
+		return symbol.fromString(symbols.get(rnd.nextInt(symbols.size())));
+	}
+
+	private static void generateSymbols() {
+		Set<String> result = new HashSet<String>();
+		
+		int i = 0;
+		while(i < SYMBOLS_SIZE) {
+			StringBuffer sb = new StringBuffer(); 
+			for (int j = 0; j < SYMBOL_LENGTH; j++) {
+				char c = letters[rnd.nextInt(letters.length)];
+				sb.append(c);
+			}
+			String symbol = sb.toString();
+			if (!result.contains(symbol)) {
+				result.add(symbol);
+				i++;
+			}
+		}
+		symbols = new ArrayList<String>(result);
 	}
 }
